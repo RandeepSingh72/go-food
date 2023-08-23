cd794d87126a4b3e8b1d827df9f710797fd8a016
Randeep Singh <101163480+RandeepSingh72@users.noreply.github.com>
2023-08-23 19:36:46 +0530

    Update MyOrder.js

--- src/screen/MyOrder.js
+++ src/screen/MyOrder.js
@@ -5,7 +5,7 @@ const MyOrder = () => {
 
   const fetchMyOrder = async () => {
     console.log(localStorage.getItem("userEmail"));
-    await fetch("http://localhost:5000/api/myOrderData", {
+    await fetch("https://mern-food-r56k.vercel.app/api/myOrderData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
