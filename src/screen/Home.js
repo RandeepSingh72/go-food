20d2dff34c397b817a6cbcba745f8b065dde3070
Randeep Singh <101163480+RandeepSingh72@users.noreply.github.com>
2023-08-23 19:35:55 +0530

    Update Home.js

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   const [foodItem, setFoodItem] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch("http://localhost:5000/api/foodData", {
+    let response = await fetch("https://mern-food-r56k.vercel.app/api/foodData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
